7c1d81e0910e34034acc1184751ddbc5d700aa02
Yishay Hazan <yishay11013@gmail.com>
2023-10-29 14:44:57 +0200

    ugly nav

--- src/common/components/navbar/index.tsx
+++ src/common/components/navbar/index.tsx
@@ -1,6 +1,27 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import styles from "./style.module.scss";
+import { FaRegCompass } from "react-icons/fa";
+import { FiUser } from "react-icons/fi";
+import { GrHomeRounded } from "react-icons/gr";
 
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.nav_wrapper}>
+      <div className={styles.nav}>
+        <Link href="/" className={styles.nav__item}>
+          <GrHomeRounded />
+          <span>Home</span>
+        </Link>
+        <Link href="/explore" className={styles.nav__item}>
+          <FaRegCompass />
+          <span>Explore</span>
+        </Link>
+        <Link href="/me" className={styles.nav__item}>
+          <FiUser />
+          <span>Profile</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
